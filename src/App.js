02db4ac16084b21b40cdcb78dcc2958ae0411145
jason1794c <69557789+jason1794c@users.noreply.github.com>
2020-10-14 10:14:43 -0400

    Aligned generators horizontally

--- src/App.js
+++ src/App.js
@@ -13,12 +13,18 @@ import dailyGrandLogo from './daily-grand-bonus-draw-3x2.png';
 function App() {
   return (
     <div className="App">
-      <img className='LottoMax' src={lottoMaxPic} alt='Lotto MAX Winning Numbers!'/> 
-      <LottoMax initialNumbers={numGen} />
-      <img className='Lotto649' src={lotto649Pic} alt='Lotto 649 Winning Numbers!' />
-      <Lotto649 initialNumbers={gen649} />
-      <img className='DailyGrand' src={dailyGrandLogo} alt='Daily Grand Winning Numbers!' />
-      <DailyGrand initialNumbers={DailyGen} />
+      <div className='column'>
+        <img className='LottoMax' src={lottoMaxPic} alt='Lotto MAX Winning Numbers!' />
+        <LottoMax initialNumbers={numGen} />
+      </div>
+      <div className='column'>
+        <img className='Lotto649' src={lotto649Pic} alt='Lotto 649 Winning Numbers!' />
+        <Lotto649 initialNumbers={gen649} />
+      </div>
+      <div className='column'>
+        <img className='DailyGrand' src={dailyGrandLogo} alt='Daily Grand Winning Numbers!' />
+        <DailyGrand initialNumbers={DailyGen} />
+      </div>
     </div>
   );
 }
