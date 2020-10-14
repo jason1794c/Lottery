c7169f126cafd57826c12e18087df2bf2bc773e5
jason1794c <69557789+jason1794c@users.noreply.github.com>
2020-10-14 08:54:56 -0400

    Added Daily Grand logo

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import lottoMaxPic from './Lotto_Max_Logo.png';
 import lotto649Pic from './1200px-Lotto_649_logo.svg.png';
 import DailyGrand from './Daily_Grand/DailyGrand';
 import DailyGen from './Daily_Grand/DailyGrandGen';
+import dailyGrandLogo from './daily-grand-bonus-draw-3x2.png';
 
 function App() {
   return (
     <div className="App">
       <img className='LottoMax' src={lottoMaxPic} alt='Lotto MAX Winning Numbers!'/> 
       <LottoMax initialNumbers={numGen} />
-      <img className='Lotto649' src={lotto649Pic} alt='Lotto 649 Winning numbers!' />
+      <img className='Lotto649' src={lotto649Pic} alt='Lotto 649 Winning Numbers!' />
       <Lotto649 initialNumbers={gen649} />
+      <img className='DailyGrand' src={dailyGrandLogo} alt='Daily Grand Winning Numbers!' />
       <DailyGrand initialNumbers={DailyGen} />
     </div>
   );
